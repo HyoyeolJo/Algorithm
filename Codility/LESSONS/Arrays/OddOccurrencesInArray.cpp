5fbeaae3d87f8f8efaded4a1a42d8a5c7ed467b6
Hyo Yeol Jo (조효열) <johyoyeol@gmail.com>
2021-04-15 07:20:42 +0900

    Update OddOccurrencesInArray.cpp

--- Codility/LESSONS/Arrays/OddOccurrencesInArray.cpp
+++ Codility/LESSONS/Arrays/OddOccurrencesInArray.cpp
@@ -8,25 +8,25 @@ int solution(vector<int> &A) {
     // write your code in C++14 (g++ 6.2.0)
     int iSum = 0;
     //element only 1
-    if( A.size() == 1U)
+    if (A.size() == 1U)
     {
         return A[0];
     }
     // A.size() is even
-    if( A.size()%2U == 0U)
+    if ((A.size() % 2U) == 0U)
     {
         return 0;
     }
-    sort(A.begin(),A.end());
-    for(unsigned int i = 0U; i < A.size(); i++)
+    sort(A.begin(), A.end());
+    for (unsigned int i = 0U; i < A.size(); i++)
     {
-        if( i%2U == 0U)
+        if ((i % 2U) == 0U)
             iSum = iSum + A[i];
         else
             iSum = iSum - A[i];
     }
 
-    if( iSum < 0)
+    if (iSum < 0)
         iSum = iSum * (-1);
     return iSum;
 }
