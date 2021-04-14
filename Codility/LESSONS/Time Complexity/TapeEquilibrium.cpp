16f221e01a36b57487e9412254182ce87be48f0c
Hyo Yeol Jo (조효열) <johyoyeol@gmail.com>
2021-04-15 07:22:42 +0900

    Update TapeEquilibrium.cpp

--- Codility/LESSONS/Time Complexity/TapeEquilibrium.cpp
+++ Codility/LESSONS/Time Complexity/TapeEquilibrium.cpp
@@ -12,26 +12,26 @@ int solution(vector<int> &A) {
     int iAbsSum = 0;
     //P = 1
     iLeftSum = A.front();
-    for(unsigned int i = 1U ; i< A.size(); i++)
+    for (unsigned int i = 1U ; i < A.size(); i++)
         iRightSum = iRightSum + A[i];
     iAbsSum = iLeftSum - iRightSum;
-    if(iAbsSum < 0)
+    if (iAbsSum < 0)
     {
         iAbsSum = iAbsSum * (-1);
     }
     iMin = iAbsSum;
 
     // P > 1
-    for(unsigned int i = 0U ; i < A.size()-2U ; i++)
+    for (unsigned int i = 0U ; i < A.size() - 2U ; i++)
     {
         iLeftSum = iLeftSum + A[i+1];
         iRightSum = iRightSum - A[i+1];
         iAbsSum = iLeftSum - iRightSum;
-        if(iAbsSum < 0)
+        if (iAbsSum < 0)
         {
             iAbsSum = iAbsSum * (-1);
         }
-        if( iMin > iAbsSum)
+        if (iMin > iAbsSum)
             iMin = iAbsSum;
     }
     return iMin;
