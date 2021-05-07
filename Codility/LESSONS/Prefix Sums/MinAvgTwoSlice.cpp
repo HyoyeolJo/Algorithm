f82ae611c3833aec5a8582966746501e3e4a341f
Hyo Yeol Jo (조효열) <johyoyeol@gmail.com>
2021-05-08 06:44:59 +0900

    Update MinAvgTwoSlice.cpp
    부분합과 최소평균의 성질을 이용하여 풀어야 한다.

--- Codility/LESSONS/Prefix Sums/MinAvgTwoSlice.cpp
+++ Codility/LESSONS/Prefix Sums/MinAvgTwoSlice.cpp
@@ -1,68 +1,48 @@
 /*
-            0 - 6
-             27
-    0 - 3           4 - 6
-      13              14
-0 - 1    2 - 3    4 - 5   6
-  6        7        6     8
-0   1    2   3    4   5
-4   2    2   5    1   5
-*/
+0 - 0 4 = 4
+0 - 1 4+2 = 6
+0 - 2 4+2+2 = 8
+0 - 3 4+2+2+5 = 13
+0 - 4 4+2+2+5+1 = 14
+0 - 5 4+2+2+5+1+5 = 19
+0 - 6 4+2+2+5+1+5+8 = 27
 
+if 1 - 2, prfix[0 - 2]- prefix[0 - 1] + A[start]
+if 1 - 3, 0 - 3까지 합에서 0 - 1 까지 합을 뺀다음 시작 원소를 더한다
+추가되는 원소가 이전 평균값보다 작을 경우에 포함하면 전체 평균이 작아진다
+*/
 // you can use includes, for example:
 // #include <algorithm>
 
 // you can write to stdout for debugging purposes, e.g.
 // cout << "this is a debug message" << endl;
-int init(int Node, int S, int E, vector<int> &A, vector<int> &B)
-{
-    if (S == E)
-    {
-        return B[Node] = A[S];
-    }
-    int Mid = (S+E)/2;
-    return B[Node] = init(Node*2, S, Mid, A, B) + init(Node*2+1, Mid+1, E, A, B);
-}
-int query(int Node, int S, int E, int L, int R, vector<int> &B)
-{
-    if (E < L || S > R)
-    {     
-        return 0;
-    }
-    if (L <= S && R >= E)
-    {
-        return B[Node];
-    }
-    int Mid = (S+E) / 2;
-    return query(Node*2, S, Mid, L, R, B) + query(Node*2+1, Mid+1, E, L, R, B);
-}
+
 int solution(vector<int> &A) {
     // write your code in C++14 (g++ 6.2.0)
-    vector<int> B;
-    B.assign(A.size()*4, 0);
-    int max = init(1,0,A.size()-1, A, B);
-    float min  = max / A.size();
-    int retX = 0;
-    int retY = 6;
-    for(unsigned int i = 0U ; i < A.size(); i++)
+    vector<int> pre_sum(A.size());
+    int pre_s = 0;
+    for(unsigned int i = 0U; i < A.size() ; i++)
     {
-        for(unsigned int j = i+1; j < A.size(); j++)
+        pre_s += A[i];
+        pre_sum[i] = pre_s;
+    }
+    int end = 1;
+    int start = 0;
+    int min_start = start;
+    float minAvg = static_cast<float>(pre_sum[end] - pre_sum[start] + A[start]) / (end-start+1);
+    for(unsigned i = 1U; i<A.size(); i++)
+    {
+        float minSubAvg = static_cast<float>(pre_sum[i] - pre_sum[start] + A[start]) / (i-start+1);
+        if (minSubAvg < minAvg)
         {
-            int P = i;
-            int Q = j;
-            int sum = 0;
-            sum = query(1,0,A.size()-1, P, Q, B);
-            float fSum = static_cast<float>(sum);
-            float fCount = static_cast<float>((Q-P) +1);
-            float avg = fSum / fCount;
-            if( min > avg)
-            {
-                min = avg;
-                retX = i;
-                retY = j;
-            }
+            minAvg = minSubAvg;
+            min_start = start;
+        }
+
+        if (A[i] < minAvg)
+        {
+            start = i;
         }
     }
-    
-    return retX;
+    return min_start;
 }
