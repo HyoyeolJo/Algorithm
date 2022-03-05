109da37477bc97813688c18544fbca22d0806712
Hyo Yeol Jo (조효열) <johyoyeol@gmail.com>
2022-03-05 22:11:28 +0900

    Create NumberOfDiscIntersections2.cpp
    https://app.codility.com/demo/results/trainingPNZP5T-94J/

---
+++ Codility/LESSONS/Sorting/NumberOfDiscIntersections2.cpp
@@ -0,0 +1,45 @@
+// you can use includes, for example:
+#include <algorithm>
+
+// you can write to stdout for debugging purposes, e.g.
+// cout << "this is a debug message" << endl;
+
+int solution(vector<int> &A) {
+    // write your code in C++14 (g++ 6.2.0)
+    vector<long>lower;
+    vector<long>upper;
+    long i = 0;
+    for(auto e : A)
+    {
+        long temp = static_cast<long>(e);
+        lower.push_back(i-temp);
+        upper.push_back(i+temp);
+        i++;
+    }
+    sort(lower.begin(),lower.end());
+    sort(upper.begin(),upper.end());
+    
+    int count = 0;
+    int j = 0;
+    int size = static_cast<int>(lower.size());
+    for(int i = 0; i < size; i++)
+    {
+        for(; j < size; j++)
+        {
+            if(lower[j] <= upper[i])
+            {
+                count+=j;
+                count-=i;
+            }
+            else {
+            {
+                break;
+            }
+            }
+        }
+    }
+
+    if(count > 10000000)
+        count = -1;
+    return count;
+}
